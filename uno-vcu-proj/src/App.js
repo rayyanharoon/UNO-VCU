a7a72cead88b64a26bcef3f1d6ea62f5d60c5d34
Nicole Tan <000431652@student.vcc.ca>
2020-11-03 16:28:24 -0800

    Added Registration in App.js

--- uno-vcu-proj/src/App.js
+++ uno-vcu-proj/src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+
+import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-  );
-}
+import Registration from './Components/Registration/Registration';
 
+var data={loginMsg: "You have successfully registered!"}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      registrationForm: [{
+        Firstname: "Isabel",
+        Lastname: "Moner",
+        Email: "isabelm@gmail.com",
+        Password: "!hello123",
+        ConfirmPassword: ""
+      },
+      {
+        Firstname: "Ant",
+        Lastname: "Nekton",
+        Email: "antInTheDeep@anorrax.com",
+        Password: "GoDeep47M",
+        ConfirmPassword: ""
+      },
+      {
+        Firstname: "Colleen",
+        Lastname: "Hoover",
+        Email: "hooverAuthor@gmail.com",
+        Password: "Author:MaybeSomeday",
+        ConfirmPassword: ""
+      }]
+    }
+  }
+
+
+
+  render() {
+    return (
+      <div className='App'>
+        <h1>Welcome to the Registration</h1>
+        First Name: <input type='text'/><br/>
+        Last Name: <input type='text'/><br/>
+        Email: <input type='text'/><br/>
+        Password: <input type='text'/><br/>
+        Confirm Password: <input type='text'/><br/>
+        <button type="submit" onClick={this.registerData}>Register</button>
+        <Registration loginMsg={data.loginMsg}/>
+      </div>
+    );
+  }
+}
 export default App;
