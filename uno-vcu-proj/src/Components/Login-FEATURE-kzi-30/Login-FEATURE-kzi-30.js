f3a88a9b7d25b22660a562a1587414c01caef782
Karina Zialcita <000428830@student.vcc.ca>
2020-11-07 09:21:27 -0800

    modified login feature

--- uno-vcu-proj/src/Components/Login-FEATURE-kzi-30/Login-FEATURE-kzi-30.js
+++ uno-vcu-proj/src/Components/Login-FEATURE-kzi-30/Login-FEATURE-kzi-30.js
@@ -1,17 +1,91 @@
 import React from 'react';
 
-class Login extends React.Component{
-    render(){
-        return (
-            <tr>
-                <td>Email: { this.props.email}</td>
-                <td>Password: {this.props.password}</td>
-            </tr>
-        )
+let data = {
+  accounts : [
+    {
+    email: "admin@vcc.ca",
+    password: "admin123"
+    },
+    {
+      email: "user@vcc.ca",
+      password: "user123"
+    }
+  ]
+}
+
+
+class Login extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      enteredAccount : {
+        email: "enteredEmail",
+        password: "enteredPassword"
+      }
+
+    }
+   
+    this.updateEmail = this.updateEmail.bind(this);
+    this.updatePassword = this.updatePassword.bind(this);
+    this.loginAccount = this.loginAccount.bind(this);
+
+  }  
+  updateEmail(e){
+    this.setState({
+      enteredAccount: {
+        ...this.state.enteredAccount,
+        email: e.target.value
+      }
+    })
+  }
+
+  updatePassword(e){
+    this.setState({
+      enteredAccount: {
+        ...this.state.enteredAccount,
+        password: e.target.value
+      }
+    })
+  }
+
+  loginAccount(){
+
+    for(let acc of data.accounts){
+      if(this.state.enteredAccount.email !== acc.email && this.state.enteredAccount.password !== acc.password)
+      {
+        console.log("invalid login")
+        return;
+      }
+      else if(this.state.enteredAccount.email === acc.email && this.state.enteredAccount.password === acc.password)
+      {
+        console.log("login matched")
+        return;
+      }
+
     }
 
 
+  }
+  render() {
+    return (
+      <div className='Login'>
+        <h1>myVCU</h1>
+        Email: <input type="text" onChange={this.updateEmail}/> <br/>
+        Password: <input type="password" onChange={this.updatePassword}/> <br/>
+        Remember me: <input type="checkbox"/> <br/>
+        <button type="submit" onClick={this.loginAccount}>Login</button> <br/>
+        <a href="#">Forgot password?</a>
 
-}   
 
-export default Login;   
+
+
+
+
+      </div>
+    )
+  }
+
+}
+
+export default Login;
