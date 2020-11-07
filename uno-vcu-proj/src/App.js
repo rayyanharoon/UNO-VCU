b4e15a404f8ae78bd2afe2c2ab167aaf18f75783
Karina Zialcita <000428830@student.vcc.ca>
2020-11-07 09:33:09 -0800

    routed Login page on App.js

--- uno-vcu-proj/src/App.js
+++ uno-vcu-proj/src/App.js
@@ -1,25 +1,30 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
-  );
+import { BrowserRouter as Router, Route, Switch, Link, Redirect} from "react-router-dom";
+
+//pages\
+import MainPage from "./components/MainPage/MainPage";
+import NotFoundPage from "./components/404/404"
+import Login from "./components/Login/Login"
+
+
+
+class App extends React.Component {
+  render(){
+    return <Router>
+      <Switch>
+      <Route exact path="/" component={MainPage}/>
+      <Route exact path="/Login" component={Login}/>
+
+
+      <Route path="/404" component={NotFoundPage}/>
+      <Redirect to="/404"/>
+      </Switch>
+    </Router>
+  }
 }
+
 
 export default App;
