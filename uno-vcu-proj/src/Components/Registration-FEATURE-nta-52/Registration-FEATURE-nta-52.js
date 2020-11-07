05660153866f3cae08e48c13e96c93adba7c093f
Nicole Tan <000431652@student.vcc.ca>
2020-11-07 15:56:40 -0800

    copied resgistration code here

--- uno-vcu-proj/src/Components/Registration-FEATURE-nta-52/Registration-FEATURE-nta-52.js
+++ uno-vcu-proj/src/Components/Registration-FEATURE-nta-52/Registration-FEATURE-nta-52.js
@@ -1,16 +1,54 @@
 import React from 'react';
+import './App.css';
 
-class Registration extends React.Component {
-    render() {
-    return (
-        <div className='App'>
-        <p>{this.props.loginMsg}</p>
-          {/* Username: <input type='text'/>
-          Password: <input type='text'/>
-          <button type="submit" onClick={this.registerData}>Register</button> */}
-        </div>
-      )
+import Registration from './Components/Registration/Registration';
+
+var data={loginMsg: "You have successfully registered!"}
+
+class Register extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      registrationForm: [{
+        Firstname: "Isabel",
+        Lastname: "Moner",
+        Email: "isabelm@gmail.com",
+        Password: "!hello123",
+        ConfirmPassword: ""
+      },
+      {
+        Firstname: "Ant",
+        Lastname: "Nekton",
+        Email: "antInTheDeep@anorrax.com",
+        Password: "GoDeep47M",
+        ConfirmPassword: ""
+      },
+      {
+        Firstname: "Colleen",
+        Lastname: "Hoover",
+        Email: "hooverAuthor@gmail.com",
+        Password: "Author:MaybeSomeday",
+        ConfirmPassword: ""
+      }]
     }
-}
+  }
 
-export default Registration;
+
+
+  render() {
+    return (
+      <div className='App'>
+        <h1>Welcome to the Registration</h1>
+        First Name: <input type='text'/><br/>
+        Last Name: <input type='text'/><br/>
+        Email: <input type='text'/><br/>
+        Password: <input type='text'/><br/>
+        Confirm Password: <input type='text'/><br/>
+        <button type="submit" onClick={this.registerData}>Register</button>
+        <Registration loginMsg={data.loginMsg}/>
+      </div>
+    );
+  }
+}
+export default Register;
