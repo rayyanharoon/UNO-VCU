4e794db4f39f6355cafe63fc1b92fdc8ac2a5dc2
Karina Zialcita <000428830@student.vcc.ca>
2020-11-07 09:34:37 -0800

    routed Login page

--- uno-vcu-proj/src/App.js
+++ uno-vcu-proj/src/App.js
@@ -4,10 +4,11 @@ import './App.css';
 
 import { BrowserRouter as Router, Route, Switch, Link, Redirect} from "react-router-dom";
 
-//pages\
-import MainPage from "./components/MainPage/MainPage";
-import NotFoundPage from "./components/404/404"
-import Login from "./components/Login/Login"
+//pages
+
+// import MainPage from "./components/MainPage/MainPage"; //Rayyan's page should be main
+// import NotFoundPage from "./components/404/404"
+import Login from "./Components/Login-FEATURE-kzi-30/Login-FEATURE-kzi-30"
 
 
 
@@ -15,10 +16,11 @@ class App extends React.Component {
   render(){
     return <Router>
       <Switch>
-      <Route exact path="/" component={MainPage}/>
+        {/* add pages here */}
+      {/* <Route exact path="/" component={Login}/> */}
       <Route exact path="/Login" component={Login}/>
 
-
+      {/* just error pages */}
       <Route path="/404" component={NotFoundPage}/>
       <Redirect to="/404"/>
       </Switch>
