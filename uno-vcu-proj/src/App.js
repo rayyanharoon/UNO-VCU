02b5fee35b281ffb76d8694f20ba16618c059f29
Karina Zialcita <000428830@student.vcc.ca>
2020-11-09 09:38:34 -0800

    added Modify room route

--- uno-vcu-proj/src/App.js
+++ uno-vcu-proj/src/App.js
@@ -9,7 +9,7 @@ import { BrowserRouter as Router, Route, Switch, Link, Redirect} from "react-rou
 import NotFoundPage from "./Components/404Page-FEATURE-kzi-30/404Page-FEATURE-kzi-30"
 import Login from "./Components/Login-FEATURE-kzi-30/Login-FEATURE-kzi-30"
 import Registration from "./Components/Registration-FEATURE-nta-52/Registration-FEATURE-nta-52";
-
+import ModifyRoom from "./Components/ModifyRoom-FEATURE-kzi-30/ModifyRoom-FEATURE-kzi-30";
 
 
 class App extends React.Component {
@@ -20,6 +20,8 @@ class App extends React.Component {
       {/* <Route exact path="/" component={MainPage}/> */}
       <Route exact path="/Login" component={Login}/>
       <Route exact path="/Registration" Component={Registration}/>
+      <Route exact path="/ModifyRoom" Component={ModifyRoom}/>
+
 
       {/* just error pages */}
       <Route path="/404" component={NotFoundPage}/>
