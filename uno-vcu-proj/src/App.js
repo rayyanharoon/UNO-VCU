268bfb8f1dbf09663540dadaccfbd77b8933aaed
Nicole Tan <000431652@student.vcc.ca>
2020-11-09 19:57:40 -0800

    edit app.js, registration.js

--- uno-vcu-proj/src/App.js
+++ uno-vcu-proj/src/App.js
@@ -8,18 +8,19 @@ import { BrowserRouter as Router, Route, Switch, Link, Redirect} from "react-rou
 // import MainPage from "./components/MainPage/MainPage"; //Rayyan's page should be main
 import NotFoundPage from "./Components/404Page-FEATURE-kzi-30/404Page-FEATURE-kzi-30"
 import Login from "./Components/Login-FEATURE-kzi-30/Login-FEATURE-kzi-30"
-import Registration from "./Components/Registration-FEATURE-nta-52/Registration-FEATURE-nta-52";
+import Register from "./Components/Registration-FEATURE-nta-52/Registration-FEATURE-nta-52";
 
 
 
 class App extends React.Component {
   render(){
+  
     return <Router>
       <Switch>
         {/* add pages here */}
       {/* <Route exact path="/" component={MainPage}/> */}
       <Route exact path="/Login" component={Login}/>
-      <Route exact path="/Registration" Component={Registration}/>
+      <Route exact path="/Registration" component={Register}/>
 
       {/* just error pages */}
       <Route path="/404" component={NotFoundPage}/>
