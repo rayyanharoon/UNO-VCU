6b4f80afeb95526abe742e1438d0e69b01dab87a
Rayyan Haroon <000427071@student.vcc.ca>
2020-11-11 20:02:51 -0800

    Search Bar addition

--- uno-vcu-proj/src/App.js
+++ uno-vcu-proj/src/App.js
@@ -5,12 +5,13 @@ import { BrowserRouter as Router, Route, Switch, Link, Redirect} from "react-rou
 
 //pages
 
-// import MainPage from "./components/MainPage/MainPage"; //Rayyan's page should be main
+import MainPage from "./Components/MainPage-FEATURE-rha-71/MainPage-FEATURE-rha-71";
 import NotFoundPage from "./Components/404Page-FEATURE-kzi-30/404Page-FEATURE-kzi-30"
 import Login from "./Components/Login-FEATURE-kzi-30/Login-FEATURE-kzi-30"
 import Register from "./Components/Registration-FEATURE-nta-52/Registration-FEATURE-nta-52";
 import ModifyRoom from "./Components/ModifyRoom-FEATURE-kzi-30/ModifyRoom-FEATURE-kzi-30";
 import ModifyAmenity from "./Components/ModifyAmenity-FEATURE-nta-52/ModifyAmenity-FEATURE-nta-52"
+import BookRooms from "./Components/BookRooms-FEATURE-rha-71/BookRooms-FEATURE-rha-71"
 
 
 class App extends React.Component {
@@ -24,6 +25,8 @@ class App extends React.Component {
       <Route exact path="/Registration-nta-52" component={Register}/>
       <Route exact path="/ModifyRoom-kzi-30" component={ModifyRoom}/>
       <Route exact path="/ModifyAmenity-nta-52" component={ModifyAmenity}/>
+      <Route exact path="/BookRooms-rha-71" component={BookRooms}/>
+      <Route exact path="/MainPage-rha-71" component={MainPage}/>
 
 
       {/* just error pages */}
