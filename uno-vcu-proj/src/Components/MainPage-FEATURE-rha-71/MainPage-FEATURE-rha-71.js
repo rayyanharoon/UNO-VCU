5b5a77a7f2376806aec0a1e87bacb8f9489fa241
Rayyan Haroon <000427071@student.vcc.ca>
2020-11-12 23:09:34 -0800

    Search bar working

--- uno-vcu-proj/src/Components/MainPage-FEATURE-rha-71/MainPage-FEATURE-rha-71.js
+++ uno-vcu-proj/src/Components/MainPage-FEATURE-rha-71/MainPage-FEATURE-rha-71.js
@@ -1,32 +1,35 @@
 import React from 'react';
 import { Component } from 'react';
+import NamesContainer from './NamesContainer';
+
 
 class MainPage extends React.Component {
     state = {
-        rooms: [
-            'Single',
-            'Double',
-            'Economy Double'
+        names: [
+          'Single', 'Double', 'Economy Double' 
         ],
         searchTerm: ''
-    }
-    editSearchTerm = (e) => {
+      }
+    
+      editSearchTerm = (e) => {
         this.setState({searchTerm: e.target.value})
-    }
-    // dynamicSearch = () => {
-    //     return this.state.names.filter(name => name.toLowerCase().includes(this.state.searchTerm.toLowerCase()));
-    // }
-
-    render() {
-        return (
+      }
+    
+      dynamicSearch = () => {
+        return this.state.names.filter(name => name.toLowerCase().includes(this.state.searchTerm.toLowerCase()))
+      }
+    
+    
+        render(){
+          return (
             <div className="App">
-                <input type='text' value = {this.state.searchTerm} onChange = {this.editSearchTerm} placeholder='Search for a room type!'/>
-                <br></br>
-                <h3>Here is a list of the room types</h3>
-                {/* <NamesContainer names={this.dynamicSearch()}/> */}
+              <input type= 'text' value = {this.state.searchTerm} onChange = {this.editSearchTerm} placeholder = 'Search for a room type'/>
+              <br></br>
+              <h3>List of Room Types:</h3>
+              <NamesContainer names = {this.dynamicSearch()}/>
             </div>
-        );
-    }
+          );
+        }
 }
 
-export default MainPage;
+export default MainPage
