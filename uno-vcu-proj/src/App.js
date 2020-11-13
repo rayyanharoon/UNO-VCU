e0ae7457a4b815f4772fc67305583c67ee729ac5
Rayyan Haroon <000427071@student.vcc.ca>
2020-11-12 23:20:34 -0800

    Rayyan's Features and Routes merged to development and added to App.js.

--- uno-vcu-proj/src/App.js
+++ uno-vcu-proj/src/App.js
@@ -9,9 +9,9 @@ import MainPage from "./Components/MainPage-FEATURE-rha-71/MainPage-FEATURE-rha-
 import NotFoundPage from "./Components/404Page-FEATURE-kzi-30/404Page-FEATURE-kzi-30"
 import Login from "./Components/Login-FEATURE-kzi-30/Login-FEATURE-kzi-30"
 import Register from "./Components/Registration-FEATURE-nta-52/Registration-FEATURE-nta-52";
+import BookRooms from "./Components/BookRooms-FEATURE-rha-71/BookRooms-FEATURE-rha-71"
 import ModifyRoom from "./Components/ModifyRoom-FEATURE-kzi-30/ModifyRoom-FEATURE-kzi-30";
 import ModifyAmenity from "./Components/ModifyAmenity-FEATURE-nta-52/ModifyAmenity-FEATURE-nta-52"
-import BookRooms from "./Components/BookRooms-FEATURE-rha-71/BookRooms-FEATURE-rha-71"
 
 
 class App extends React.Component {
@@ -20,13 +20,12 @@ class App extends React.Component {
     return <Router>
       <Switch>
         {/* add pages here */}
-      {/* <Route exact path="/" component={MainPage}/> */}
+      <Route exact path="/" component={MainPage}/>
       <Route exact path="/Login-kzi-30" component={Login}/>
       <Route exact path="/Registration-nta-52" component={Register}/>
       <Route exact path="/ModifyRoom-kzi-30" component={ModifyRoom}/>
       <Route exact path="/ModifyAmenity-nta-52" component={ModifyAmenity}/>
       <Route exact path="/BookRooms-rha-71" component={BookRooms}/>
-      <Route exact path="/MainPage-rha-71" component={MainPage}/>
 
 
       {/* just error pages */}
