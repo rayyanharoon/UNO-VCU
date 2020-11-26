8697b68e5da196b5b22593cfc075308d35309a9a
Karina Zialcita <000428830@student.vcc.ca>
2020-11-26 08:57:28 -0800

    WaitList Feature

---
+++ uno-vcu-proj/src/Components/Waitlist-FEATURE-kzi-30/Waitlist-FEATURE-kzi-30.js
@@ -0,0 +1,53 @@
+
+
+import React from "react";
+import {Link} from "react-router-dom";
+
+
+class WaitList extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.state = {};
+    }
+
+    signUp = () => {
+        console.log("Submit button was called");
+        let data = {
+            name: document.getElementById("nameInput").value,
+            email: document.getElementById("emailInput").value,
+            phone: document.getElementById("phoneInput").value,
+            address: document.getElementById("addressInput").value,
+
+        }
+        fetch('http://localhost:8000/WaitList',
+        {
+            method:'POST',
+            headers:{'Content-type' : 'Application/json'},
+            body: JSON.stringify({data})
+        })
+        .then(response =>response.json)
+        .then(data => this.setState({"data":data}))
+    }
+    render(){
+        return(
+        <div className="App">
+            <h1>Sign up for the Waitlist!</h1>
+            <br/>
+            Full Name: <input type="text" id="nameInput"/><br/>
+            Email: <input type="text" id="emailInput"/><br/>
+            Phone Number: <input type="text" id="phoneInput"/><br/>
+            Address: <input type="text" id="addressInput"/>
+            <br/>
+            <button onClick={this.signUp}>Submit</button>
+            <br/>
+            <Link to="/MainPage"><button type="button">Go to Main</button></Link>
+
+
+
+        </div>
+)
+    }
+}
+
+export default WaitList;
