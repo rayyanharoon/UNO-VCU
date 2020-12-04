44a67affcaaf10e440f7ef5071a63519eada8740
Rayyan Haroon <000427071@student.vcc.ca>
2020-12-03 20:52:12 -0800

    InfoPage incorporated inside App.js

---
+++ uno-vcu-proj/src/Components/InfoPage-FEATURE-rha-71/InfoPage-FEATURE-rha-71.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Component } from 'react';
+
+class InfoPage extends Component {
+    render() {
+        return (
+            <div className="app">
+            
+                <h1>Welcome To Vancouver Community University</h1>
+
+            </div>  
+            )
+        
+    }
+}
+
+export default InfoPage;
